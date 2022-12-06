737a6c44a48668f6f73b5c691d4e225b407e5bbf
Stephen Maina <stevohvosti@gmail.com>
2022-12-06 11:56:38 +0300

    build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+
+  const [ result, setResult] = useState("");
+
+  const clickHandle = (e) => {
+    setResult(result.concat(e.target.value))
+  }
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    setResult("")
+  }
+
+  const calculate = (e) => {
+    setResult(eval(result).toString())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calc">
+      <input type="text" placeholder='0'  id="answer" value={result}/>
+      <input type="button" value={"0"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"1"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"2"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"3"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"4"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"5"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"6"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"7"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"8"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"9"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"*"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"+"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"-"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"."} className="button" onClick={clickHandle}/>
+      <input type="button" value={"/"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"%"} className="button" onClick={clickHandle}/>
+      <input type="button" value={"DEL"} onClick={handleDelete}  className="button1"/>
+      <input type="button" value={"="} onClick={calculate} className="button1"/>
+   
+
     </div>
   );
 }
